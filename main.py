16adbf4eb63216535a38675de6d384975e89fa88
Keith <Keith.Bossert@verizon.net>
2020-12-07 08:26:11 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import csv
+
+def finddups():
+
+    try:
+        with open('USArchives.csv', newline='', encoding="utf8") as csvfile:
+            name = ""
+            count = 0
+
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if name == row['repository_name_unauthorized']:
+                        print(row['repository_name_unauthorized'], row['state'])
+                        count = count + 1
+                name = row['repository_name_unauthorized']
+
+            print(count)
+    except:
+        print("Unable to open the USArchives.csv file")
+
+def findunique():
+
+    try:
+        with open('USArchives.csv', newline='', encoding="utf8") as csvfile:
+            name = ""
+            count = 0
+
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if name != row['repository_name_unauthorized']:
+                        print(row['repository_name_unauthorized'], row['state'])
+                        count = count + 1
+                name = row['repository_name_unauthorized']
+
+            print(count)
+    except:
+        print("Unable to open the USArchives.csv file")
+
+def findauthorized():
+
+    try:
+        with open('USArchives.csv', newline='', encoding="utf8") as csvfile:
+            count = 0
+
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['repository_name_authorized']:
+                    print(row['repository_name_authorized'], row['state'])
+                    count = count + 1
+
+            print(count)
+    except:
+        print("Unable to open the USArchives.csv file")
+
+findauthorized()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Let's Go")
+
